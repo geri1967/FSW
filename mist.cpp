f245ec72e37f453093fc2f53496cf52e2d71f372
geri1967 <86065645+geri1967@users.noreply.github.com>
2021-06-18 20:05:24 +0700

    Add files via upload
    First Commit

---
+++ mist.cpp
@@ -0,0 +1,27 @@
+#include <conio.h>
+#include<stdlib.h>
+#include <iostream>
+
+void mistery(int x)
+{
+	if(x%15==0)
+	  printf("fizzbuzz");
+	else if(x%3==0)  
+	  printf("fizz");
+	    else if(x%5==0)  
+	           printf("buzz");
+	          else printf(" ");
+}
+
+int main()
+{
+   int jwb=0,x;
+   while (jwb==0)
+   {
+   	  system("cls");
+      printf("Masukkan nilai X= ");scanf("%d",&x);
+      mistery(x);
+      printf("\n\nMau Ulang Lagi(0))= ");scanf("%d",&jwb);
+	  //scanf("%c",&jwb);
+   }
+}
